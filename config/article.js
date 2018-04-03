d7c5f03d5f18efc3b735f13f0b9f52b29e5a9817
Cale Tilford <cale.tilford@ft.com>
2018-04-03 11:52:31 +0100

    Update twitter image size

--- config/article.js
+++ config/article.js
@@ -65,7 +65,7 @@ export default (environment = 'development') => ({
         Twitter card docs:
         https://dev.twitter.com/cards/markup
   */
-  twitterCard: 'summary',
+  twitterCard: 'summary_large_image',
 
   /*
   TODO: Do you want to tweak any of the
